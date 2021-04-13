7065e0c31ae0c314a1c60e6c4e988b4bb092f7b7
Piotr <pm@piotrmichna.pl>
2021-04-13 10:12:51 +0200

    define BUSS_NAME data

--- homster/items/management/commands_data/items_data.py
+++ homster/items/management/commands_data/items_data.py
@@ -1,3 +1,12 @@
 ITEMS_NAME = [
     ['raspberry', 'mikrokomputer (ARM 11 1GHz 512MB RAM)', 40],
 ]
+
+BUSS_NAME = [
+    ['raspberry', 'GPIO', 'Piny wejścia i wyścia'],
+    ['raspberry', 'I2C0', 'Szeregowa szyna danych'],
+    ['raspberry', 'I2C1', 'Szeregowa szyna danych'],
+    ['raspberry', 'SPI0', 'Szeregowa szyna danych'],
+    ['raspberry', 'Supply', 'Złącza zasilania'],
+    ['raspberry', 'UART0', 'Szeregowa szyna danych'],
+]
