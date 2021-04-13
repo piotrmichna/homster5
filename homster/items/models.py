03113f99a003c293f6b877de5f85a2cca3c2d818
Piotr <pm@piotrmichna.pl>
2021-04-13 10:00:40 +0200

    define models calss ItemName

--- homster/items/models.py
+++ homster/items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ItemName(models.Model):
+    class Meta:
+        verbose_name = 'Sterownik'
+        verbose_name_plural = '1 Sterowniki'
+        ordering = ['name']  # Sort in asc order
+
+    name = models.CharField(max_length=16, verbose_name='Nazwa sterownika')
+    description = models.CharField(max_length=64, verbose_name='Opis')
+    num_pin = models.PositiveSmallIntegerField(verbose_name='Ilość pinów IO')
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
