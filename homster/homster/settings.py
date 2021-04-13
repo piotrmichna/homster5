1bc5b69dcd785074808d823fd8f0c7b74696f3f7
Piotr <pm@piotrmichna.pl>
2021-04-13 19:03:25 +0200

    test3

--- homster/homster/settings.py
+++ homster/homster/settings.py
@@ -111,10 +111,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    'admin/',
 ]
 
 try:
