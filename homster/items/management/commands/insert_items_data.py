16d520b5462679dbd47026f35e7d7f8ab445760e
Piotr <pm@piotrmichna.pl>
2021-04-13 10:22:25 +0200

    define function insert_buss_pin()

--- homster/items/management/commands/insert_items_data.py
+++ homster/items/management/commands/insert_items_data.py
@@ -1,6 +1,6 @@
 # from django.core.management import BaseCommand
-from items.management.commands_data.items_data import (ITEMS_NAME, BUSS_NAME)
-from items.models import (ItemName, BussNameType)
+from items.management.commands_data.items_data import (ITEMS_NAME, BUSS_NAME, BUSS_PIN)
+from items.models import (ItemName, BussNameType, BussPinType)
 
 
 def insert_items_name():
@@ -15,3 +15,14 @@ def insert_buss_name():
         for i, n, d in BUSS_NAME:
             if i == item_n.name:
                 BussNameType.objects.create(items_name=item_n, name=n, description=d)
+
+
+def insert_buss_pin():
+    items_names = ItemName.objects.values_list('name', flat=True)
+    for item_name in items_names:
+        item_n = ItemName.objects.get(name=item_name)
+        for i, n, d, p, b in BUSS_PIN:
+            if i == item_n.name:
+                buss = BussNameType.objects.get(name=b)
+                BussPinType.objects.create(items_name=item_n, buss_name=buss, name=n,
+                                           description=d, pin_board=p)
