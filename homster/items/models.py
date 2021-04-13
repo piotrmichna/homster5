abafd33bd185684560c7c6f34a62dd90c150511b
Piotr <pm@piotrmichna.pl>
2021-04-13 10:02:47 +0200

    define models calss BussNameType

--- homster/items/models.py
+++ homster/items/models.py
@@ -13,3 +13,18 @@ class ItemName(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.description}'
+
+
+class BussNameType(models.Model):
+    class Meta:
+        verbose_name = 'Magistrala'
+        verbose_name_plural = '2 Magistrale'
+        ordering = ['items_name__name', 'name']
+
+    items_name = models.ForeignKey(ItemName, on_delete=models.CASCADE, verbose_name='Urządzenie')
+    name = models.CharField(max_length=16, verbose_name='Magistrala')
+    description = models.CharField(max_length=48, verbose_name='Opis')
+    buss_adr = models.CharField(max_length=8, default=None, null=True, verbose_name='Adres komunikacji')
+
+    def __str__(self):
+        return f'{self.items_name.name} | {self.name} | {self.description}'
