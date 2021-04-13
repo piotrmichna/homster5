cb71f95bb2ccea342d43b8be1e082ba68178b044
Piotr <pm@piotrmichna.pl>
2021-04-13 12:33:12 +0200

    register programs models for django admin site

--- homster/programs/admin.py
+++ homster/programs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from programs.models import ProgName, ProgStartTime, ProgPinCfg
+
+admin.site.register(ProgName)
+admin.site.register(ProgStartTime)
+admin.site.register(ProgPinCfg)
