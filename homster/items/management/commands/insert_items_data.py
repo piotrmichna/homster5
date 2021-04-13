316ece088ff8608fd04032ddf16c332d4403d766
Piotr <pm@piotrmichna.pl>
2021-04-13 10:20:59 +0200

    define function insert_buss_name()

--- homster/items/management/commands/insert_items_data.py
+++ homster/items/management/commands/insert_items_data.py
@@ -6,3 +6,12 @@ from items.models import (ItemName, BussNameType)
 def insert_items_name():
     for n, d, np in ITEMS_NAME:
         ItemName.objects.create(name=n, description=d, num_pin=np)
+
+
+def insert_buss_name():
+    items_names = ItemName.objects.values_list('name', flat=True)
+    for item_name in items_names:
+        item_n = ItemName.objects.get(name=item_name)
+        for i, n, d in BUSS_NAME:
+            if i == item_n.name:
+                BussNameType.objects.create(items_name=item_n, name=n, description=d)
