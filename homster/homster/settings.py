716aeffacff18c1f49570baeb17b394794d1fbf6
Piotr <pm@piotrmichna.pl>
2021-04-13 18:07:36 +0200

    xxx

--- homster/homster/settings.py
+++ homster/homster/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'homster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../items/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
