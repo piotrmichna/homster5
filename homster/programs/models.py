1865f5c339c4eb0b572f854056949db1a154822e
Piotr <pm@piotrmichna.pl>
2021-04-13 12:05:35 +0200

    define class model ProgName

--- homster/programs/models.py
+++ homster/programs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ProgName(models.Model):
+    class Meta:
+        verbose_name = 'Program'
+        verbose_name_plural = '5 Programy'
+        ordering = ['active', 'name']
+
+    name = models.CharField(max_length=32, verbose_name="Nazwa programu")
+    description = models.CharField(max_length=48, null=True, verbose_name='Opis programu')
+    active = models.BooleanField(default=True, verbose_name='Aktywny')
+
+    def __str__(self):
+        if self.active:
+            return f'{self.name} - {self.description} | (Aktywny)'
+        else:
+            return f'{self.name} - {self.description} | (Wyłączony)'
