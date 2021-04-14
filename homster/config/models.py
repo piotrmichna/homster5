dfabca2d465ebf49531e0260d7d46bf2af160b6a
Piotr <pm@piotrmichna.pl>
2021-04-14 06:08:19 +0200

    define class model CfgType

--- homster/config/models.py
+++ homster/config/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CfgType(models.Model):
+    class Meta:
+        verbose_name = 'Typ komendy'
+        verbose_name_plural = 'Typy komend'
+        ordering = ['name']
+
+    name = models.CharField(max_length=16, verbose_name="Komenda")
+    description = models.CharField(max_length=128, null=True, verbose_name='Opis polecenia')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
