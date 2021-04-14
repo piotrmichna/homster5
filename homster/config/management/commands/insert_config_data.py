cf89cc0d2256bc85839150165ec95409207c795a
Piotr <pm@piotrmichna.pl>
2021-04-14 07:09:46 +0200

    define function insert_cfg_command()

--- homster/config/management/commands/insert_config_data.py
+++ homster/config/management/commands/insert_config_data.py
@@ -6,3 +6,9 @@ def insert_cfg_type():
     for n, d in CFG_TYPE_DATA:
         CfgType.objects.create(name=n, description=d)
 
+
+def insert_cfg_command():
+    for tn, n, d, v in CFG_COMMAND_DATA:
+        tp = CfgType.objects.get(name=tn)
+        CfgCommand.objects.create(type=tp, name=n, description=d, value=v)
+
