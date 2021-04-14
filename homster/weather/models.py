71c5b48d69f2365de8affd5cd3ce1e4efc0ed7ae
Piotr <pm@piotrmichna.pl>
2021-04-14 14:00:41 +0200

    define WeatherLong model

--- homster/weather/models.py
+++ homster/weather/models.py
@@ -11,4 +11,28 @@ class WeatherDayly(models.Model):
     temp_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Temperatura')
     pres_m = models.PositiveSmallIntegerField(verbose_name='Ciśnienie atmosferyczne')
     humi_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Wilgotność powietrza')
-    sunl_m = models.PositiveSmallIntegerField(verbose_name='Nasłonecznienie')
+    ligh_m = models.PositiveSmallIntegerField(verbose_name='Nasłonecznienie')
+
+
+class WeatherLong(models.Model):
+    class Meta:
+        verbose_name = 'Pogoda'
+        verbose_name_plural = 'Pogoda'
+        ordering = ['time_m']
+
+    temp_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Temperatura')
+    pres_m = models.PositiveSmallIntegerField(verbose_name='Ciśnienie atmosferyczne')
+    humi_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Wilgotność powietrza')
+    ligh_m = models.PositiveSmallIntegerField(verbose_name='Nasłonecznienie')
+
+    time_day_start = models.TimeField(verbose_name='Czas rozpoczęcia dnia')
+    time_day_stop = models.TimeField(verbose_name='Czas zakończenia dnia')
+
+    temp_day_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Temperatura')
+    pres_day_m = models.PositiveSmallIntegerField(verbose_name='Ciśnienie atmosferyczne')
+    humi_day_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Wilgotność powietrza')
+    ligh_day_m = models.PositiveSmallIntegerField(verbose_name='Nasłonecznienie')
+    temp_night_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Temperatura')
+    pres_night_m = models.PositiveSmallIntegerField(verbose_name='Ciśnienie atmosferyczne')
+    humi_night_m = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Wilgotność powietrza')
+    ligh_day_m = models.PositiveSmallIntegerField(verbose_name='Nasłonecznienie')
