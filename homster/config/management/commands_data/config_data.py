c468522958a60d63ada6395af4d497a58d7ebd13
Piotr <pm@piotrmichna.pl>
2021-04-14 06:57:36 +0200

    define CFG_COMMAND_DATA

--- homster/config/management/commands_data/config_data.py
+++ homster/config/management/commands_data/config_data.py
@@ -4,3 +4,11 @@ CFG_TYPE_DATA = [
     ['wthr_mens', 'Ustawienia pomiaru pogody'],
 ]
 
+CFG_COMMAD_DATA = [
+    ['syst', 'rb_sys', 'Restart systemu', 'False'],
+    ['syst', 'pwr_off_sys', 'Wyłączenie systemu', 'False'],
+    ['syst', 'updt_sys', 'Aktualizacja oprogramowania', 'False'],
+    ['prog', 'chk_cmd', 'Rozdzielczość wykonywania poleceń [s]', '1'],
+    ['wthr_mens', 'chk_sns', 'Rozdzielczość wykonywania pomiarów [s]', '15'],
+    ['wthr_mens', 'sv_sns', 'Rozdzielczość zapisu pomiarów [m]', '15'],
+]
