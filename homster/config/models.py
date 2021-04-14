34cd2af3f3c5cb1b122aa1fd5425d56770a997d5
Piotr <pm@piotrmichna.pl>
2021-04-14 06:41:48 +0200

    modyfi CfgType models and migrate change to db

--- homster/config/models.py
+++ homster/config/models.py
@@ -7,8 +7,8 @@ class CfgType(models.Model):
         verbose_name_plural = 'Typy komend'
         ordering = ['name']
 
-    name = models.CharField(max_length=16, verbose_name="Komenda")
-    description = models.CharField(max_length=128, null=True, verbose_name='Opis polecenia')
+    name = models.CharField(max_length=8, verbose_name="Komenda")
+    description = models.CharField(max_length=64, null=True, verbose_name='Opis polecenia')
 
     def __str__(self):
         return f'{self.name} ({self.description})'
