c61fd4d7867b7961ad9924875156bf7403040e15
Piotr <pm@piotrmichna.pl>
2021-04-14 07:38:05 +0200

    test and modyfi verbos name

--- homster/config/models.py
+++ homster/config/models.py
@@ -4,23 +4,26 @@ from django.db import models
 class CfgType(models.Model):
     class Meta:
         verbose_name = 'Typ komendy'
-        verbose_name_plural = 'Typy komend'
+        verbose_name_plural = 'Konfiguracja - typy komend'
         ordering = ['name']
 
-    name = models.CharField(max_length=16, verbose_name="Komenda")
-    description = models.CharField(max_length=64, null=True, verbose_name='Opis polecenia')
+    description = models.CharField(max_length=64, null=True, verbose_name='Opis typu polecenia')
+    name = models.CharField(max_length=16, verbose_name="Nazwa typu polecenia")
 
     def __str__(self):
-        return f'{self.name} ({self.description})'
+        return f'{self.description} ({self.name})'
 
 
 class CfgCommand(models.Model):
     class Meta:
         verbose_name = 'Konfiguracja'
         verbose_name_plural = 'Konfiguracja - komendy'
-        ordering = ['name']
+        ordering = ['type__description', 'name']
 
+    description = models.CharField(max_length=64, null=True, verbose_name='Opis polecenia')
     type = models.ForeignKey(CfgType, on_delete=models.CASCADE, verbose_name='Typ komendy')
     name = models.CharField(max_length=16, verbose_name='Komenda')
-    description = models.CharField(max_length=64, null=True, verbose_name='Opis polecenia')
     value = models.CharField(max_length=16, verbose_name='Wartość')
+
+    def __str__(self):
+        return f'{self.type.description} - {self.description} ({self.value})'
