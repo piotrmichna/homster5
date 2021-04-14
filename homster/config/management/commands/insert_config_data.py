ad87113a0f56cf542824a502c8a5b928f7c99959
Piotr <pm@piotrmichna.pl>
2021-04-14 07:22:38 +0200

    cleaning db table before adding data

--- homster/config/management/commands/insert_config_data.py
+++ homster/config/management/commands/insert_config_data.py
@@ -4,6 +4,11 @@ from config.management.commands_data.config_data import CFG_TYPE_DATA, CFG_COMMA
 from config.models import CfgType, CfgCommand
 
 
+def delete_cfg_all():
+    CfgCommand.objects.all().delete()
+    CfgType.objects.all().delete()
+
+
 def insert_cfg_type():
     for n, d in CFG_TYPE_DATA:
         CfgType.objects.create(name=n, description=d)
@@ -19,6 +24,7 @@ class Command(BaseCommand):
     help = 'Wstawienie danych konfiguracji do bazy.'
 
     def handle(self, *args, **options):
+        delete_cfg_all()
         insert_cfg_type()
         insert_cfg_command()
         print('Pomyślnie dodano polecenia konfiguracji do bazy.')
