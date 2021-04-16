59d8ed54982744985e9c12a35877413b374255e6
Piotr <pm@piotrmichna.pl>
2021-04-16 09:11:19 +0200

    register urls to view config wether endpoint

--- homster/restapp/urls.py
+++ homster/restapp/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import SimpleRouter
 
-from restapp.views import (WeatherDailyViewSet, WeatherLongViewSet, WeatherWeekViewSet)
+from restapp.views import (WeatherDailyViewSet, WeatherLongViewSet, WeatherWeekViewSet, CfgWeatherViewSet)
 
 router = SimpleRouter()
 
 router.register('weather/daily', WeatherDailyViewSet, basename='weather-daily')
 router.register('weather/long', WeatherLongViewSet, basename='weather-long')
 router.register('weather/week', WeatherWeekViewSet, basename='weather-week')
+router.register('cfg/weather', CfgWeatherViewSet, basename='cfg-weather')
 
 urlpatterns = router.urls
