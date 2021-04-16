3dff1c1cee5674aea8eba1766c44f5682b29d6c7
Piotr <pm@piotrmichna.pl>
2021-04-16 08:43:25 +0200

    define ViewSet for WeatherWeek models serializer

--- homster/restapp/views.py
+++ homster/restapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from restapp.serializers import WeatherDailySerializer, WeatherLongSerializer
-from weather.models import WeatherDaily
+from restapp.serializers import (WeatherDailySerializer, WeatherLongSerializer, WeatherWeekSerializer)
+from weather.models import (WeatherDaily, WeatherWeek)
 
 
 class WeatherDailyViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,8 @@ class WeatherDailyViewSet(viewsets.ModelViewSet):
 class WeatherLongViewSet(viewsets.ModelViewSet):
     serializer_class = WeatherLongSerializer
     queryset = WeatherDaily.objects.all()
+
+
+class WeatherWeekViewSet(viewsets.ModelViewSet):
+    serializer_class = WeatherWeekSerializer
+    queryset = WeatherWeek.objects.all()
