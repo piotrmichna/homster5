b47ae8a0e3724cf5f32214d6bf56d7db74275b02
Piotr <pm@piotrmichna.pl>
2021-04-16 08:42:54 +0200

    define WeatherWeek serializer

--- homster/restapp/serializers.py
+++ homster/restapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from weather.models import (WeatherDaily, WeatherLong)
+from weather.models import (WeatherDaily, WeatherLong, WeatherWeek)
 
 
 class WeatherDailySerializer(serializers.ModelSerializer):
@@ -19,6 +19,28 @@ class WeatherDailySerializer(serializers.ModelSerializer):
 class WeatherLongSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeatherLong
+        fields = (
+            'date_m',
+            'temp_m',
+            'pres_m',
+            'humi_m',
+            'ligh_m',
+            'time_day_start',
+            'time_day_stop',
+            'temp_day_m',
+            'pres_day_m',
+            'humi_day_m',
+            'ligh_day_m',
+            'temp_night_m',
+            'pres_night_m',
+            'humi_night_m',
+            'ligh_night_m',
+        )
+
+
+class WeatherWeekSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeatherWeek
         fields = (
             'date_m',
             'temp_m',
