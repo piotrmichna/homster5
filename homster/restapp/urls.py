28cdcf6cbed78910472ee93c8f32e927b7848e8f
Piotr <pm@piotrmichna.pl>
2021-04-16 08:38:48 +0200

    register path to WeatherLongViewSets

--- homster/restapp/urls.py
+++ homster/restapp/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from restapp.views import WeatherDailyViewSet
+from restapp.views import (WeatherDailyViewSet, WeatherLongViewSet)
 
 router = SimpleRouter()
 
 router.register('weather/daily', WeatherDailyViewSet, basename='weather-daily')
+router.register('weather/long', WeatherLongViewSet, basename='weather-long')
 
 urlpatterns = router.urls
