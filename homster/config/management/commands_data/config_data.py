8e90c35e3e6ce4143aecc7f46fc4f2c39f29cf74
Piotr <pm@piotrmichna.pl>
2021-04-21 23:13:33 +0200

    adding data command in config management data

--- homster/config/management/commands_data/config_data.py
+++ homster/config/management/commands_data/config_data.py
@@ -1,17 +1,22 @@
 CFG_TYPE_DATA = [
-    ['syst', 'Ustawienia systemowe'],
     ['prog', 'Ustawienia programów'],
+    ['sync', 'Synchronizacja'],
     ['wthr', 'Ustawienia pomiaru pogody'],
+    ['syst', 'Ustawienia systemowe'],
 ]
 
 CFG_COMMAND_DATA = [
-    ['syst', 'rb_sys', 'Restart systemu', 'False'],
-    ['syst', 'pwr_off_sys', 'Wyłączenie systemu', 'False'],
-    ['syst', 'updt_sys', 'Aktualizacja oprogramowania', 'False'],
     ['prog', 'chk_cmd', 'Rozdzielczość wykonywania poleceń [s]', '1'],
     ['wthr', 'chk_sns', 'Rozdzielczość wykonywania pomiarów [s]', '15'],
     ['wthr', 'sv_sns', 'Rozdzielczość zapisu pomiarów [m]', '15'],
     ['wthr', 'new_sns', 'Wysłany nowy pomiar pogody', 'False'],
     ['wthr', 'week_sns', 'Zapis średnich tygodniowych', 'True'],
     ['wthr', 'long_sns', 'Zapis średnich dziennych', 'True'],
+    ['sync', 'itf_nprog', 'Nowe dane programu z interfejsu', 'False'],
+    ['sync', 'itf_ngpio', 'Nowe dane gpio z interfejsu', 'False'],
+    ['sync', 'hdw_nprog', 'Nowe dane programu z urządzenia', 'False'],
+    ['sync', 'hdw_ngpio', 'Nowe dane gpio z urządzenia', 'False'],
+    ['syst', 'rb_sys', 'Restart systemu', 'False'],
+    ['syst', 'pwr_off_sys', 'Wyłączenie systemu', 'False'],
+    ['syst', 'updt_sys', 'Aktualizacja oprogramowania', 'False'],
 ]
