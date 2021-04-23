8218cbfb05ed353e4d760d9295f9c2bdc1f28639
Piotr <pm@piotrmichna.pl>
2021-04-23 21:37:32 +0200

    modify config management commang

--- homster/config/management/commands/insert_config_data.py
+++ homster/config/management/commands/insert_config_data.py
@@ -1,12 +1,14 @@
 from django.core.management import BaseCommand
+from django.db import connection
 
 from config.management.commands_data.config_data import CFG_TYPE_DATA, CFG_COMMAND_DATA
 from config.models import CfgType, CfgCommand
 
 
 def delete_cfg_all():
-    CfgCommand.objects.all().delete()
-    CfgType.objects.all().delete()
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE TABLE config_cfgcommand RESTART IDENTITY CASCADE")
+    cursor.execute("TRUNCATE TABLE config_cfgtype RESTART IDENTITY CASCADE")
 
 
 def insert_cfg_type():
@@ -18,6 +20,14 @@ def insert_cfg_command():
     for tn, n, d, v in CFG_COMMAND_DATA:
         tp = CfgType.objects.get(name=tn)
         CfgCommand.objects.create(type=tp, name=n, description=d, value=v)
+
+
+def insert_command_all():
+    print('Usuwanie zawartości tabel aplikacji config')
+    delete_cfg_all()
+    print('Dodanie zawartości tabel aplikacji config')
+    insert_cfg_type()
+    insert_cfg_command()
 
 
 class Command(BaseCommand):
