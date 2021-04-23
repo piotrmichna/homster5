284b96964c0cef567d8f6a718801d9c690f8ee41
Piotr <pm@piotrmichna.pl>
2021-04-23 06:23:05 +0200

    register url tras for Program Starts endpoint

--- homster/restapp/urls.py
+++ homster/restapp/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import SimpleRouter
 
 from restapp.views import (WeatherDailyViewSet, WeatherLongViewSet, WeatherWeekViewSet, CfgWeatherViewSet,
                            CfgSystemViewSet, CfgProgramViewSet, ProgramsCfgViewSet, ProgPinCfgViewSet,
-                           GpioPinCfgViewSet, SyncViewSet)
+                           GpioPinCfgViewSet, SyncViewSet, ProgStartTimeViewSet)
 
 router = SimpleRouter()
 
@@ -13,6 +13,7 @@ router.register('cfg/weather', CfgWeatherViewSet, basename='cfg-weather')
 router.register('cfg/system', CfgSystemViewSet, basename='cfg-system')
 router.register('cfg/program', CfgProgramViewSet, basename='cfg-program')
 router.register('cfg/prog_name', ProgramsCfgViewSet, basename='cfg-prog_name')
+router.register('cfg/prog_start', ProgStartTimeViewSet, basename='cfg-prog_start')
 router.register('cfg/prog_pin', ProgPinCfgViewSet, basename='cfg-prog_pin')
 router.register('cfg/gpio_pin', GpioPinCfgViewSet, basename='cfg-gpio_pin')
 router.register('sync', SyncViewSet, basename='sync')
