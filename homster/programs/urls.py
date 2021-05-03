2ca4d1865405bb32e22ee946e76bc955713501b6
Piotr Michna <pm@piotrmichna.pl>
2021-05-03 20:59:11 +0200

    adding url tras for progrmas aplication

--- homster/programs/urls.py
+++ homster/programs/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from programs.views import ProgramslView
-from weather.views import WeatherChartsView
+from programs.views import (ProgramslView, ProgramShowView, ProgramRemoveView, ProgramItemsAddView,
+                            ProgramItemsEditView, ProgramStartAddView, ProgramStartEditView)
 
 urlpatterns = [
     path('', ProgramslView.as_view(), name='programs'),
+    path('remove/<int:pr_id>/', ProgramRemoveView.as_view(), name='program-remove'),
+    path('edit/<int:pr_id>/', ProgramShowView.as_view(), name='program-edit'),
+
+    path('start_add/<int:pr_id>/', ProgramStartAddView.as_view(), name='program-start-add'),
+    path('start_edit/<int:pr_id>/<int:st_id>/', ProgramStartEditView.as_view(), name='program-start-edit'),
+
+    path('item_add/<int:pr_id>/', ProgramItemsAddView.as_view(), name='program-item-add'),
+    path('item_edit/<int:pr_id>/<int:it_id>/', ProgramItemsEditView.as_view(), name='program-item-edit'),
 ]
