53bf61055d5d48bd996ae9830a9b393220f6e9bc
Piotr Michna <pm@piotrmichna.pl>
2021-05-03 22:35:13 +0200

    adding url tras to test weather stats

--- homster/weather/urls.py
+++ homster/weather/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from weather.views import WeatherChartsView
+from weather.views import WeatherChartsView, WeatherCreateDayView
 
 urlpatterns = [
     path('', WeatherChartsView.as_view(), name='weather'),
+    path('data/', WeatherCreateDayView.as_view(), name='weather'),
 ]
